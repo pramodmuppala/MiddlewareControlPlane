5fb731b076e3eff4b3402d0980362fac223529ed
preddyzion <pmuppala@gmail.com>
2026-04-09 22:59:29 -0400

    updated with evidence

--- setllm.sh
+++ setllm.sh
@@ -1,3 +1,3 @@
-export OPENAI_API_KEY='REDACTED_OPENAI_KEY'
+export OPENAI_API_KEY=''
 export OPENAI_MODEL='gpt-4.1-mini'
-export export GEMINI_API_KEY='AIzaSyCaieyTX_Cw7WlcPDbO6SxvBy9DsdTai10' 
+export export GEMINI_API_KEY='' 
